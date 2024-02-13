fb41a19b04d4a673e633292e51d53e1acdf1f91d
Rosynant <arrobasin@gmail.com>
2024-02-13 18:40:46 +0100

    Add project files.

---
+++ MathOrWords/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MathOrWords
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MathOrWords/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MathOrWords
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
