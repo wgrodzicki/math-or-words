e7324b26cf725bffffbc699e470fcc8c5058165f
Rosynant <arrobasin@gmail.com>
2024-02-18 13:04:32 +0100

    Added table attributes to the Game class, created GameRepository class for db queries

---
+++ MathOrWords/Data/GameRepository.cs
@@ -0,0 +1,39 @@
+﻿using SQLite;
+using MathOrWords.Models;
+
+namespace MathOrWords.Data;
+
+public class GameRepository
+{
+    private string _dbPath;
+    private SQLiteConnection _connection;
+
+    public GameRepository(string dbPath)
+    {
+        _dbPath = dbPath;
+    }
+
+    public void Init()
+    {
+        _connection = new SQLiteConnection(_dbPath);
+        _connection.CreateTable<Game>(); // Creates a table if it doesn't exist
+    }
+
+    public List<Game> GetAllGames()
+    {
+        Init();
+        return _connection.Table<Game>().ToList();
+    }
+
+    public void AddGame(Game game)
+    {
+        _connection = new SQLiteConnection(_dbPath);
+        _connection.Insert(game);
+    }
+
+    public void DeleteGame(int id)
+    {
+        _connection = new SQLiteConnection(_dbPath);
+        _connection.Delete(new { Id = id });
+    }
+}
