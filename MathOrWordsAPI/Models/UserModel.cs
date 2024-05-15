fa63bd088a906b1192a794fd2530ddb5d476b88e
Rosynant <arrobasin@gmail.com>
2024-05-15 18:45:09 +0200

    Added the 1st controller, data context and connection string

---
+++ MathOrWordsAPI/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MathOrWordsAPI.Models;
+
+public class UserModel
+{
+    public int Id { get; set; }
+    public required string Login { get; set; }
+    public required string Password { get; set; }
+    public string Email { get; set; } = string.Empty;
+}
