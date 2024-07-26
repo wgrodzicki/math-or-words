7fc40923ef1bbda67e25d162d7d337a5981b4727
Wojciech Grodzicki <109021468+wgrodzicki@users.noreply.github.com>
2024-07-26 18:11:41 +0200

    Updated readme

--- MathOrWordsAPI/Models/UserModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MathOrWordsAPI.Models;
-
-public class UserModel
-{
-    public int Id { get; set; }
-    public required string Login { get; set; }
-    public required string Password { get; set; }
-    public string Email { get; set; } = string.Empty;
-}
